c393a542e0dbe161ef858117858a2d1c6cf5537c
ange1020 <angelina.30571079@uru.edu>
2025-06-21 13:13:12 -0400

    feat: implementación de método para buscar por categoría

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -1,6 +1,7 @@
 
 import  tmdbApi  from '../utils/tmdbApi.js';
 import categoryModel from '../models/category.js';
+import movieModel from '../models/movie.js'
 import { AppError } from '../utils/appError.js';
 
 class SearchService {
@@ -64,14 +65,41 @@ class SearchService {
 
 
     async searchByCategories(body) {
+
         const { categories } = body;
         
         if (!categories || categories.length === 0) throw new AppError("Las categorías son requeridas", 400);
         
-        const categoryData = categoryModel.findByFilter({ _id: { $in: categories } })
+        const categoryData = await categoryModel.findByFilter({ _id: { $in: categories } })
 
-        console.log(categoryData);
-        
+        const categoriesId = categoryData.map(cat => cat._id)
+        const categoriesTmbdId = categoryData.map(cat => cat.tmdb_id).join('|')
+
+        // const movies = await movieModel.findByFilter({categories:{ $in: categoriesId}})
+
+        const res = await tmdbApi.discoverMovies({genres: categoriesTmbdId })
+
+        const customResult ={
+            totalItems: res.total_results,
+            currentPage: res.page,
+            totalPages: res.total_pages,
+            results: res.results.map(movie => ({
+                id: movie.id,
+                title: movie.title,
+                release_date: movie.release_date,
+                poster_path: movie.poster_path || movie.backdrop_path,
+                user_rating: 0,
+                critic_rating: 0,
+                total_rating: 0,
+            }))
+        }
+
+        if (customResult.totalItems === 0) {
+            throw new AppError("No se encontraron películas con ese nombre", 404);
+        }
+
+        return customResult;
+
     }
 }
 export default new SearchService();
